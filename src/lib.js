0a0d9725a6fcb15056290822b3244601565ee01f
Srushti <bsrushti@in-bsrushti.local>
2018-12-26 12:26:44 +0530

    Introduces getCharacterCount function, Added test cases

--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,12 @@
 const getLineCount = function(content) {
-    return content.split('\n').length;
+  return content.split("\n").length;
+};
+
+const getCharacterCount = function(content) {
+  return content.length; 
 };
 
 module.exports = {
-    getLineCount
-}
+  getLineCount,
+  getCharacterCount
+};
--- test/libTest.js
+++ test/libTest.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const { getLineCount } = require("../src/lib");
+const { getLineCount, getCharacterCount } = require("../src/lib");
 
 describe("getLineCount", function() {
   it("should return 1 when empty content provided", function() {
@@ -17,6 +17,26 @@ describe("getLineCount", function() {
   it("should return line count as per the given content with multiple \\n", function() {
     const actual = getLineCount("abc\nefg\nhij\nklm\nnop");
     const expected = 5;
+    assert.deepEqual(actual, expected);
+  });
+});
+
+describe("getCharacterCount", function() {
+  it("should return 0 when empty content provided", function() {
+    const actual = getCharacterCount("");
+    const expected = 0;
+    assert.deepEqual(actual, expected);
+  });
+
+  it("should return 2 when content has two characters ", function() {
+    const actual = getCharacterCount("ab");
+    const expected = 2;
+    assert.deepEqual(actual, expected);
+  });
+
+  it("should return byte count as per the given content", function() {
+    const actual = getCharacterCount("abc\nefg\nhij\nklm\nnop");
+    const expected = 19;
     assert.deepEqual(actual, expected);
   });
 });
