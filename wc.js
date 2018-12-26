8109eb8862f44e1b3060e417af410621e081e602
Srushti <bsrushti@in-bsrushti.local>
2018-12-26 15:00:15 +0530

    Introduces main function

---
+++ wc.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const {runCommand} = require('./src/lib');
+
+const main = function(){
+  const params = process.argv.slice(2);
+  console.log(runCommand(fs, params));
+}
+
+main();
