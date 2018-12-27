91184da5d1f9d63975377e6977bcdfdcd0395827
Srushti <bsrushti@in-bsrushti.local>
2018-12-27 09:53:43 +0530

    Introduces separate file for constants

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const NEWLINE = "\n";
+const EMPTY_STRING = "";
+const ENCODING_FORMAT = "utf8";
+const HYPHEN = "-";
+
+module.exports = {
+  NEWLINE,
+  EMPTY_STRING,
+  ENCODING_FORMAT,
+  HYPHEN
+};
